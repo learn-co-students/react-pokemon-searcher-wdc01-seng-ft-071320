009b8b5b12d4c2de63395684b9d4c7da3e0f5940
chsohn15 <chsohn15@gmail.com>
2020-09-18 17:19:43 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,17 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+
+  makePokemon = () => {
+    return this.props.pokemons.map(pokemon => (<PokemonCard pokemon={pokemon} />))
+  }
+  
   render() {
+    
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.makePokemon()}
       </Card.Group>
     )
   }
