04b735bc8a6fda23b77ad5f1ea0f807c4ffcb38b
ClaFiore <claborghini@gmail.com>
2020-09-19 11:09:03 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -4,39 +4,12 @@ import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
 
-  
-  state = {
-    pokemons: []
-  }
-  
-  componentDidMount(){
-    fetch('http://localhost:3000/pokemon')
-    .then(res => res.json())
-    .then(pokemonArray => {
-      let newpokeArray = pokemonArray.map(poke => poke = {...poke, imgFront: true})
-      this.setState({
-        pokemons: newpokeArray
-      })
-    })
-  }
-
-  displayImage = (clickedPokemon) => {
-    let newPokemonArray = this.state.pokemons.map(pokemon => {
-        if(pokemon === clickedPokemon){
-          return {...pokemon, imgFront: !pokemon.imgFront}
-        }
-        return pokemon
-      })
-      
-    this.setState({
-      pokemons: newPokemonArray
-    })
-  }
 
   render() {
+    console.log(this.props)
     return (
       <Card.Group itemsPerRow={6}>
-        {this.state.pokemons.map(pokemon => <PokemonCard pokemon={pokemon}  displayImage={this.displayImage}/>)}
+        {this.props.pokemons.map(pokemon => <PokemonCard pokemon={pokemon}  displayImage={this.props.displayImage}/>)}
       </Card.Group>
     )
   }
