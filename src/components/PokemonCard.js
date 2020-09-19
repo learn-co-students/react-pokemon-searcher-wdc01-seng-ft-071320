7db6aca4b8d9f9bea8de70336ae57aa5c1e75cd9
solomonfy <fantahunsolomon@gmail.com>
2020-09-19 01:07:51 -0400

    render all pokemons except image

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
   render() {
+    let pokemon = this.props;
+    // console.log(this.props);
     return (
       <Card>
-        <div>
+        <div onClick={() => this.props.toggleImage(pokemon.id)}>
           <div className="image">
-            <img alt="oh no!" />
+            <img src="pokemon.sprites.back" alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {pokemon.hp}
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import Search from './Search'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import Search from "./Search";
+import { Container } from "semantic-ui-react";
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemonsArray: [],
+    isClicked: false,
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then((resp) => resp.json())
+      .then((listOfPokemons) =>
+        this.setState({
+          pokemonsArray: listOfPokemons,
+        })
+      );
+  }
+
+  toggleImage = (id) => {
+    // console.log(id);
+    let foundPok = this.state.pokemonsArray.filter((pok) => pok.id === id);
+    this.setState({
+      pokemonsArray: [...this.state.pokemonsArray, foundPok],
+    });
+  };
+
   render() {
+    // console.log(this.state.pokemonsArray);
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
@@ -14,10 +38,13 @@ class PokemonPage extends React.Component {
         <br />
         <Search />
         <br />
-        <PokemonCollection />
+        <PokemonCollection
+          allpokemons={this.state.pokemonsArray}
+          toggleImage={this.toggleImage}
+        />
       </Container>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
